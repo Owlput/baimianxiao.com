863accf6c403a5e5ff35ed54e3e0b5c45a3bebdd
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2021-09-24 23:20:20 +0800

    [UPD]:2021.9.24:001

--- src/components/ImageDiv.js
+++ src/components/ImageDiv.js
@@ -7,22 +7,51 @@ export default class ImageDiv extends React.Component {
     super(props);
     this.state = {
       currentPage: 1,
-      maxPage: 3,
+      maxPage: 0,
+      imageAll: [],
       imagesOnPage: [],
     };
   }
+
   componentDidMount() {
     fetch(`http://localhost:7000/data/imageAllThumbs`)
       .then((response) => response.json())
       .then((result) => {
-        this.setState({ imagesOnPage: result.thumbs });
+        this.setState({
+          imageAll: result.thumbs,
+          maxPage: Math.ceil(result.thumbs.length / 5),
+          imagesOnPage: result.thumbs.slice(
+            (this.state.currentPage - 1) * 5,
+            this.state.currentPage * 5
+          ),
+        });
       });
   }
+
   prePage = () => {
-    
+      console.log(`current ${this.state.currentPage}`)
+    if(this.state.currentPage > 1){
+      console.log(`from page ${this.state.currentPage} to ${this.state.currentPage -1}`)
+    this.setState({
+      currentPage: this.state.currentPage -1,
+      imagesOnPage: this.state.imageAll.slice((this.state.currentPage -2) * 5, (this.state.currentPage-1) * 5 ),
+    });}
+    console.log(`now on page ${this.state.currentPage}`)
   };
   nextPage = () => {
+    console.log(`current ${this.state.currentPage}`)
+    if(this.state.currentPage < this.state.maxPage ){
+      console.log(`from page ${this.state.currentPage} to ${this.state.currentPage +1}`)
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+      imagesOnPage: this.state.imageAll.slice((this.state.currentPage) * 5, (this.state.currentPage +1) * 5 ),
+    });
+    console.log(`now on page ${this.state.currentPage}`)
+  }
   };
+  currentInfo = () =>{
+    console.log(`Now page ${this.state.currentPage} with ${this.state.imagesOnPage.length} elements`)
+  }
   render() {
     return (
       <div style={componentWarpperStyle}>
@@ -35,6 +64,7 @@ export default class ImageDiv extends React.Component {
           <button style={buttonStyle} onClick={this.prePage}>
             上一页
           </button>
+          <button onClick={this.currentInfo}> Current Status </button>
           <button style={buttonStyle} onClick={this.nextPage}>
             下一页
           </button>
@@ -47,8 +77,8 @@ export default class ImageDiv extends React.Component {
 const imageTabsStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(5,18em)",
-  gridTemplateRows: "repeat(5,18em)",
-  gap: "1em",
+  gridTemplateRows: "repeat(1,18em)",
+  gap: "0.5em",
 };
 
 const GenericFlexWarpperStyle = {
