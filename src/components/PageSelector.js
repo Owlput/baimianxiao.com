3c0094918d627cd33d1f0c5a7f965114ff6a394e
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2021-10-04 23:13:30 +0800

    新增页面选择部件（待完善）

--- src/components/PageSelector.js
+++ src/components/PageSelector.js
@@ -1,4 +1,26 @@
 import React from "react";
+import arrow from '../assets/icons/chevron-left.svg'
+import '../assets/css/pageSelector.css'
+
+
 export default function PageSelector() {
-  return <></>;
+
+
+  return (<div>
+        <button className="button">
+        <img src={arrow} style={arrowStyle.left} alt="Previous page button"></img>
+        </button>
+        <button className="button">
+        <img src={arrow} style={arrowStyle.right}alt="Next page button" ></img>
+        </button>
+  </div>);
+}
+const arrowStyle = {
+  left:{
+    width:"2em",
+  },
+  right:{
+    width:"2em",
+    transform:"rotate(180deg)",
+  },
 }
